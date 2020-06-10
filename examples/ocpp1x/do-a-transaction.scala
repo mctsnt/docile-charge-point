56b52b73a1ebac0ff57b6c58a998c407e03338be
Stefan Post <s.post@thenewmotion.com>
2020-06-10 14:10:17 +0200

    Provide a more detailed example

--- examples/ocpp1x/do-a-transaction.scala
+++ examples/ocpp1x/do-a-transaction.scala
@@ -4,17 +4,54 @@ val auth = authorize(chargeTokenId).idTag
 if (auth.status == AuthorizationStatus.Accepted) {
 
   statusNotification(status = ChargePointStatus.Occupied(Some(OccupancyKind.Preparing)))
-  val transId = startTransaction(meterStart = 300, idTag = chargeTokenId).transactionId
-  statusNotification(status = ChargePointStatus.Occupied(Some(OccupancyKind.Charging)))
+
+  val startTransactionTimestamp = java.time.ZonedDateTime.now
+  val stopTransactionTimestamp = startTransactionTimestamp.plusMinutes(10)
+  val connectorScope = ConnectorScope(0) // meaning the 1st EVSE
+
+  val transId = startTransaction(
+    meterStart = 300,
+    idTag = chargeTokenId,
+    connector = connectorScope,
+    timestamp = startTransactionTimestamp
+  ).transactionId
+
+  statusNotification(
+    status = ChargePointStatus.Occupied(Some(OccupancyKind.Charging)),
+    scope = connectorScope,
+    timestamp = Some(startTransactionTimestamp)
+  )
 
   prompt("Press ENTER to stop charging")
 
-  statusNotification(status = ChargePointStatus.Occupied(Some(OccupancyKind.Finishing)))
-  stopTransaction(meterStop = 310, transactionId = transId, idTag = Some(chargeTokenId))
-  statusNotification(status = ChargePointStatus.Available())
+  // sleep(randomize(1.second))
+  // statusNotification(
+  //   status = ChargePointStatus.Faulted(
+  //     errorCode = Some(com.thenewmotion.ocpp.messages.v1x.ChargePointErrorCode.OtherError),
+  //     vendorErrorCode = Some("349")
+  //   ),
+  //   scope = connectorScope,
+  //   timestamp = Some(stopTransactionTimestamp)
+  // )
 
+  statusNotification(
+    status = ChargePointStatus.Occupied(Some(OccupancyKind.Finishing)),
+    scope = connectorScope,
+    timestamp = Some(stopTransactionTimestamp)
+  )
+
+  stopTransaction(
+    transactionId = transId,
+    meterStop = 310,
+    idTag = Some(chargeTokenId),
+    timestamp = stopTransactionTimestamp
+  )
+
+  statusNotification(
+    status = ChargePointStatus.Available(),
+    scope = connectorScope,
+    timestamp = Some(stopTransactionTimestamp)
+  )
 } else {
   fail("Not authorized")
 }
-
-// vim: set ts=4 sw=4 et:
